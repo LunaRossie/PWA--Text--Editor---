29e1d76e1bd216823853de6b73f52eb734758c0a
LunaRossie <>
2022-08-14 14:55:50 -0400

    add to files

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -48,10 +48,10 @@ module.exports = () => {
 
     module: {
       rules: [
-        {
-					test: /\.css$/i,
-					use: ['style-loader', 'css-loader'],
-				},
+              {
+					      test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+        },
 				{
 					test: /\.m?js$/,
 					exclude: /node_modules/,
